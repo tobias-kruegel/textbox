16fbd9a929eb4f91faec1947292ae22a910127a9
Tobias Krügel <mail@tobias-kruegel.de>
2020-01-19 18:48:53 +0100

    wip

--- src/app/textbox/textbox.component.ts
+++ src/app/textbox/textbox.component.ts
@@ -25,21 +25,23 @@ export class TextboxComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     this.lineHeight = Math.floor(parseFloat(getComputedStyle(this.textarea.nativeElement, null).getPropertyValue('line-height')));
-    // this.textarea.nativeElement.style.height = this.lineHeight + 'px';
-    // this.textarea.nativeElement.style.fontSize = (this.lineHeight - 4) + 'px';
-    // this.textarea.nativeElement.style.lineHeight = this.lineHeight + 'px';
+    this.textarea.nativeElement.style.lineHeight = this.lineHeight + 'px';
+    this.textarea.nativeElement.style.fontSize = (this.lineHeight - 4) + 'px';
+    this.textarea.nativeElement.style.height = this.lineHeight + 'px';
     this.textarea.nativeElement.value = this.value;
   }
 
   public onInput() {
-    this.textarea.nativeElement.setAttribute('rows', '1');
-    // this.textarea.nativeElement.style.height = (this.lineHeight + 4) + 'px';
+    // this.textarea.nativeElement.setAttribute('rows', '1');
+    console.log(this.textarea.nativeElement.scrollHeight);
+    this.textarea.nativeElement.style.height = (this.lineHeight) + 'px';
     let rows = Math.round(this.textarea.nativeElement.scrollHeight / this.lineHeight);
     if (rows > this.maxRows) {
       rows = this.maxRows;
     }
-    this.textarea.nativeElement.setAttribute('rows', rows.toString());
-    // this.textarea.nativeElement.style.height = (rows * this.lineHeight + 4) + 'px';
+    // this.textarea.nativeElement.setAttribute('rows', rows.toString());
+    this.textarea.nativeElement.style.height = (rows * this.lineHeight) + 'px';
+    // this.textarea.nativeElement.style.padding = '4px';
     this.isError = this.textarea.nativeElement.value.length > this.maxLength;
   }
 
